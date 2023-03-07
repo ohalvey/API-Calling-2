cbf55c90c4cdb1b2e19a4dd3399a5089e3caaba1
oliver halvey <oliverhalvey310@yahoo.com>
2023-03-07 07:58:40 -0800

    Initial Commit

---
+++ API Calling 2/API_Calling_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_2App.swift
+//  API Calling 2
+//
+//  Created by Oliver Halvey on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Calling 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API Calling 2
+//
+//  Created by Oliver Halvey on 3/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
